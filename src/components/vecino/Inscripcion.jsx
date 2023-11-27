ada60940cdd2beea637762d9add7f4f30e524954
MartinTorresg <mart.torresg@duocuc.cl>
2023-11-27 15:25:19 -0300

    implementado rechazarInscripcion con correo

--- src/components/vecino/Inscripcion.jsx
+++ src/components/vecino/Inscripcion.jsx
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom';
 import { Global } from '../../helpers/Global';
 import { Peticion } from '../../helpers/Peticion';
 import { Register } from '../user/Register';
+import { useNavigate } from 'react-router-dom';
 
 export const Inscripcion = () => {
     const [inscripcion, setInscripcion] = useState({});
     const [cargando, setCargando] = useState(true);
     const params = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         conseguirInscripcion();
@@ -22,6 +24,47 @@ export const Inscripcion = () => {
 
         setCargando(false);
     };
+
+    const rechazarInscripcion = async () => {
+        try {
+            // Llamada al backend para eliminar la inscripción
+            const response = await fetch(Global.url + "inscripcion/borrar-inscripcion/" + params.id, {
+                method: "DELETE"
+            });
+    
+            const data = await response.json();
+    
+            if (data.status === "success") {
+                // Enviar correo electrónico de rechazo
+                enviarCorreoRechazo(data.inscripcion.email, "Su inscripción ha sido rechazada.");
+                navigate('/admin/inscripciones');
+            } else {
+                // Manejar errores
+                console.log("Error al eliminar la inscripción");
+            }
+        } catch (error) {
+            console.error("Error en rechazarInscripcion: ", error);
+        }
+    };
+    
+    const enviarCorreoRechazo = async (email, mensaje) => {
+        // Implementar la lógica para enviar un correo electrónico de rechazo
+        const correoData = {
+            email: email,
+            mensaje: mensaje
+        };
+    
+        const response = await fetch(Global.url + "inscripcion/enviarCorreo", {
+            method: "POST",
+            body: JSON.stringify(correoData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    
+        const data = await response.json();
+        // Manejar respuesta del servidor
+    };    
 
     return (
         <div className='jumbo'>
@@ -38,6 +81,7 @@ export const Inscripcion = () => {
                 </>
             }
             {!cargando && <Register datosInscripcion={inscripcion} />}
+            <button onClick={rechazarInscripcion} className='btn btn-danger'>Rechazar</button>
         </div>
     );
 };
